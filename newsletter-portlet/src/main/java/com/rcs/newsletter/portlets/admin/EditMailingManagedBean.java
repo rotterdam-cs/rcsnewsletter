4f5e3e85c0f7efdf8fdba80b844b1577a34b7f98
juan <juan@mendoza22>
2011-11-14 12:29:54 -0300

    fixed tab logic on admin.xhtml updated ui for mailing and fixed web scopes on the spring framework

---
+++ newsletter-portlet/src/main/java/com/rcs/newsletter/portlets/admin/EditMailingManagedBean.java
@@ -0,0 +1,14 @@
+package com.rcs.newsletter.portlets.admin;
+
+import javax.faces.bean.ManagedBean;
+import org.springframework.context.annotation.Scope;
+
+/**
+ *
+ * @author juan
+ */
+@ManagedBean
+@Scope("session")
+public class EditMailingManagedBean {
+    
+}

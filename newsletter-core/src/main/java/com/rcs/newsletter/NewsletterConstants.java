eff5948add7ec6ab92288ca2655e632e8f26ae35
Ariel Parra <ariel@rotterdam-cs.com>
2011-11-17 16:48:18 -0300

    Fixing email content

--- newsletter-core/src/main/java/com/rcs/newsletter/NewsletterConstants.java
+++ newsletter-core/src/main/java/com/rcs/newsletter/NewsletterConstants.java
@@ -11,4 +11,7 @@ public interface NewsletterConstants {
     public static final String ALGORITHM_SHA1 = "SHA1";    
     public static final String NEWSLETTER_ADMIN = "admin@liferay.com";
     
+    public static final String CONFIRMATION_LINK_TOKEN = "{LINK}";
+    public static final String LIST_NAME_TOKEN = "{LIST}";
+    
 }
